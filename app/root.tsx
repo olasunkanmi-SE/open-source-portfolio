59364b822798745d3c693dee62c1568e7b7b69ec
Olasunkanmi Oyinlola <olasunkanmioyinlola@Olasunkanmis-Mac-mini.local>
2024-07-08 10:56:41 +0800

    chore(degbugging): Update dependencies to latest versions
    * Update  to version 5.3.3
    * Update  to version 2.10.4
    * Update other dependencies to their latest versions
    
    This commit aims to ensure the project remains up-to-date and compatible with the latest dependencies.

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,14 @@
-import {
-  Form,
-  Links,
-  Meta,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Links, Meta, Scripts, ScrollRestoration } from "@remix-run/react";
+import { LinksFunction } from "@remix-run/server-runtime";
+import styles from "./style.css?url";
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css",
+  },
+  { rel: "stylesheet", href: styles },
+];
 
 export default function App() {
   return (
@@ -16,35 +20,6 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-
         <ScrollRestoration />
         <Scripts />
       </body>
