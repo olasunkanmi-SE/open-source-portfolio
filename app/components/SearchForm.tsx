43c896ce126375fc1c9e8d2868adf97c89fa815a
Olasunkanmi Oyinlola <olasunkanmioyinlola@Olasunkanmis-Mac-mini.local>
2024-07-09 22:00:16 +0800

    Added a new SearchForm component with basic functionality Created a new PostCreationForm for managing post creation Updated the CodeBuddy route to use the new components Introduced new dependencies and fonts for enhanced user experience

---
+++ app/components/SearchForm.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+interface SearchFormProps {
+  onSearch: (searchTerm: string) => void;
+  buttonText?: string;
+  placeholder?: string;
+  buttonVariant?: string;
+}
+
+const SearchForm: React.FC<SearchFormProps> = ({ onSearch, placeholder = "Search" }) => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSearch(searchTerm);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit} style={{ fontSize: "14px", fontWeight: 300 }} className="d-flex">
+      <Form.Control
+        type="search"
+        placeholder={placeholder}
+        className="me-2"
+        aria-label="Search"
+        value={searchTerm}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)}
+      />
+    </Form>
+  );
+};
+
+export default SearchForm;
