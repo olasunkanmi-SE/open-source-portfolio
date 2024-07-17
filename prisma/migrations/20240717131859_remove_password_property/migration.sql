7c514c89b0a8103670035a8801a25467aaa2e8c3
Olasunkanmi Oyinlola <olasunkanmioyinlola@Olasunkanmis-Mac-mini.local>
2024-07-17 21:28:34 +0800

    fix(db): implement user authentication with hashed passwords
    Added db.server.ts file to create a Prisma client instance
    Implemented user repository with CRUD operations (create, read, update, delete)
    Updated utils/utils.ts to include hash and verify password functions
    Migrated User table to store password hash instead of plain password
    Created additional migration scripts to add and remove passwordHash column
    Refactored User model to use passwordHash instead of password

---
+++ prisma/migrations/20240717131859_remove_password_property/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "name" TEXT
+);
+INSERT INTO "new_User" ("email", "id", "name", "passwordHash") SELECT "email", "id", "name", "passwordHash" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
